085ac5eb1b71ca9dfb968889956d5b362e5ded70
djelka243 <jessydjonga23@gmail.com>
2025-12-25 19:33:11 +0100

    “url”

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "674731451"
+    serviceWorkerVersion: "3651512186"
   }
 });
